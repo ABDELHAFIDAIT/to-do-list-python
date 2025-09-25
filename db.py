ebb294208bb82690f4ccfb69ea5d7973fd2d417c
ABDELHAFIDAIT <abdelhafid02002@gmail.com>
2025-09-25 11:25:06 +0100

    add db.py that create a connection with DB and add Tasks Table

---
+++ db.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import (create_engine, Table, Column, MetaData, Integer, String, Text, Enum)
+
+load_dotenv()
+
+# import .env variables
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+# create an engine
+engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+
+metadata = MetaData()
+
+# create Tables
+tasks = Table(
+    "tasks",
+    metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("title", String, nullable=False),
+    Column("description", Text, nullable=False),
+    Column("priority", Enum("P1", "P2", "P3", name="enum_priority"), server_default="P1"),
+    Column("status", Enum("Doing", "Done", name="enum_status"), server_default="Doing")
+)
+
+# add the Tasks Table in DB
+try :
+    metadata.create_all(engine)
+    print("Table Added Succefully !")
+except Exception as e :
+    print("Error :\n", e)
